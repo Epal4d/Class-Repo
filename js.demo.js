08ad858b182611768a4cf8d3d1a2b9cdc179d100
evan.al4d <evan.al4d@gmail.com>
2025-02-17 14:58:07 -0600

    add demo js page

---
+++ js.demo.js
@@ -0,0 +1,17 @@
+ // single line comment 
+
+        /* multi 
+        line 
+        comment 
+        */
+
+        //group code in a name called function 
+        function display_info_1()
+        {
+
+
+        console.log("# JS demo 2");
+            //alert ("# Js-demo-2")
+        }
+        // call function
+        display_info_1();
